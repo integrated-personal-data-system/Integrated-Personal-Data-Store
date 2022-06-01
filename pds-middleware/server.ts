4ca441b2e536c671d077284419cc9578f885a171
Casey Rock <rockc@udel.edu>
2022-06-01 10:12:22 -0400

    added node sever temple to docker compose

--- pds-middleware/server.ts
+++ pds-middleware/server.ts
@@ -1,8 +1,8 @@
 import express, { Express, Handler, Request, Response } from "express"
-
 const app = express();
 const port = 8080;
 
 app.listen(port, () => {
     console.log(`Aries PDS Middleware software listening on port ${port}`)
 });
+
