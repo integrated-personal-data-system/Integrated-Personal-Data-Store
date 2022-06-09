4df74d00b7eb231448024a57f5ef6281a001994d
Ubuntu <ubuntu@ip-172-31-19-79.us-east-2.compute.internal>
2022-06-09 18:31:55 +0000

    added tls certs to server

--- pds-middleware/server/server.ts
+++ pds-middleware/server/server.ts
@@ -1,17 +1,31 @@
 import express, { Request, Response } from "express";
 import readMyData from "./sparql/readMydata";
 import path from "path"
+import fs from "fs"
+var https = require('https');
+
 
 const app = express();
-const port = 8080;
+
+
+let certificate = fs.readFileSync("/etc/letsencrypt/live/iamtestingbed.com/cert.pem", 'utf8');
+let privateKey = fs.readFileSync("/etc/letsencrypt/live/iamtestingbed.com/privkey.pem", 'utf8');
+
+
+let credentials = {
+    key: privateKey,
+    cert: certificate
+};
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(443, () => {
+    console.log(`Aries PDS Middleware software listening on port ${443}`);
+});
+
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-
-app.listen(port, () => {
-    console.log(`Aries PDS Middleware software listening on port ${port}`);
-});
-
 
 
 app.get('/readMyData', (request: Request, response: Response) => {
